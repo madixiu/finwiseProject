57b68d46ceef3e5c532ffb466bdd47edc97b27bb
madixiu <mahdi.moradi72@gmail.com>
2020-12-26 12:38:40 +0330

    fixing users db migration

--- backend/scripts/start_prod.sh
+++ backend/scripts/start_prod.sh
@@ -2,6 +2,8 @@
 
 #cd backend
 python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations users
+python3 manage.py migrate users
 python3 manage.py makemigrations
 python3 manage.py migrate --no-input
 gunicorn backend.wsgi -b 0.0.0.0:8000
