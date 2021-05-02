01f97babed61e275d836392a2b51c161856ec401
Madix <mahdi.moradi72@gmail.com>
2021-05-02 16:13:49 +0430

    changed the SSL cert

--- backend/scripts/start_asgi.sh
+++ backend/scripts/start_asgi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-daphne backend.asgi:application --bind 0.0.0.0 --port 9000
+# daphne backend.asgi:application --bind 0.0.0.0 --port 9000
 
 #daphne SSL
-# sudo daphne -e ssl:443:privateKey=privkey.pem:certKey=fullchain.pem backend.asgi:application
+daphne -e ssl:443:privateKey=key.pem:certKey=cert.pem backend.asgi:application --port 9000
