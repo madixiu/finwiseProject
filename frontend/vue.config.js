37b4dba68ed1350efcba8973e7e936ce052245b6
Madix <mahdi.moradi72@gmail.com>
2021-07-25 22:42:55 +0430

    +vue.config sourceMap false for production

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  // productionSourceMap: false,
   publicPath:
     // process.env.NODE_ENV === "production" ? "/metronic/vue/demo1/" : "/",
     "/",
